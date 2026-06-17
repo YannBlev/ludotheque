f160b538d1caef54c02b381b085f671e6615c8bc
YannBlev <36kbrl@gmail.com>
2026-06-17 13:11:35 +0200

    [S1001, S1002, S1003]

---
+++ src/main/java/fr/eni/ludotheque/bo/Client.java
@@ -0,0 +1,31 @@
+package fr.eni.ludotheque.bo;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@Table(name="CLIENT")
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Data
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "no_client")
+    private Long id;
+
+    @NonNull
+    private String nom;
+
+    @NonNull
+    private String prenom;
+
+    @NonNull
+    private String email;
+
+    @Column(name = "no_telephone")
+    private String telephone;
+}
